a400b88092235a18f4d41f412cf366ebf1322ead
Dung246 <dungoo2605@gmail.com>
2026-03-05 14:33:38 +0700

    Refactor Bt2 class and implement counting logic
    Refactored class name and package structure, added input handling and counting logic.

--- src/b2/Bt2.java
+++ src/b2/Bt2.java
@@ -1,5 +1,61 @@
-package b2;
+package b1;
+import java.util.*;
 
-public class Bt2 {
+public class BT2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("loi: so luong khong hop le");
+            return;
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+
+            if (map.containsKey(x)) {
+                map.put(x, map.get(x) + 1);
+            } else {
+                map.put(x, 1);
+            }
+        }
+
+        List<Integer> keys = new ArrayList<>(map.keySet());
+
+        for (int i = 0; i < keys.size() - 1; i++) {
+            for (int j = i + 1; j < keys.size(); j++) {
+                if (keys.get(i) > keys.get(j)) {
+                    int t = keys.get(i);
+                    keys.set(i, keys.get(j));
+                    keys.set(j, t);
+                }
+            }
+        }
+
+        int max = 0;
+
+        for (int k : keys) {
+            if (map.get(k) > max) {
+                max = map.get(k);
+            }
+        }
+
+        for (int k : keys) {
+            System.out.println(k + " xuat hien: " + map.get(k) + " lan");
+        }
+
+        System.out.print("So xuat hien nhieu nhat: ");
+
+        for (int k : keys) {
+            if (map.get(k) == max) {
+                System.out.print(k + " ");
+            }
+        }
+
+        System.out.println("(" + max + " lan)");
+    }
 }
