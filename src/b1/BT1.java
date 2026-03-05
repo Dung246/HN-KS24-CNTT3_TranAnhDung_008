59669d1e957224665be61b99471996416ad58bed
Dung246 <dungoo2605@gmail.com>
2026-03-05 14:07:06 +0700

    load

---
+++ src/b1/BT1.java
@@ -0,0 +1,23 @@
+package b1;
+//Xây dựng chương trình quản lí danh sách tên sinh vien.Người dùng nhâp số  lượng sinh viên và danh sách tên
+//Yêu cầu dùng list<String> để lưu dữ liệu,phải tự viết thuật toán xử lí,chương trình cần thực hiện : tìm tên dài nhất (nếu cùng độ dài thì lấy tên xuất hiện đầu tiên),hiển thị danh sách tên theo thứ tự alphabet ,đếm số sinh viên có tên bắt đầubằng chữ "A"(không phân bit hoa/thường)
+//không ti các khoảng trắng đầu,cuối chuỗi
+//TH hợp lệ : Input :5,An,Bình,Anh,Cuong,Ha
+//output longest name:Cuong
+//sortList An Binh Cuong Ha
+//Number ofname starting with A: 2
+//TH N không hợp lệ : Input 0/output: lỗi : số lượng sinh viên không hợp lệ
+
+// b2: sử dụng map+thuật toán  xây dựng chương trình thống kê tần suất xuất hiện của các số trong một dãy s nguyên nhập từ bàn phím
+//yêu cầu :sử dụng map<Integer,Interger> để luu: key(số nguyên),value9laaf xuất hiện),tự viết thuật toán
+//tìm số xuất hiện nhiều nhất (nếu nhiều thì liệt kê),sắp xếp các số theo thứ tự tăng dần
+//input : 10(số lượng) 10, 2 3 4 2 3 5 2 3 3 ,output:  2 xuất hiện :3 lần,3 xuất hiện 4 lần,4 xuất hiện 1 lần,5 xuất hiện 1 lần, 10 xuất hiện 1 lần, só xuất hiện nhiều nhất  3(4 lần)
+
+//B3: sử dụng stack  viết chương trình chuyển đổi một số thập phn (không âm nguyên) sang nhị phân bằng cách sử dụng stack
+//phải dùng stack<integer> lưu số dư ,tự viết thuật toán chuyển đổi,in ra kết quả nhị phân tương ứng
+// input : 10 output :1010
+//input :0 output   : 0
+//input : 5 output: lôi : số không hợp lệ
+public class BT1 {
+
+}
